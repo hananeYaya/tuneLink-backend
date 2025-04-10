f7e4bb922acd5e62d279613c56c70fb4390141e7
jael99 <etswakajenis@gmail.com>
2025-04-11 01:27:21 +0200

    on going

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -2,21 +2,26 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-from app.database import Base  # Le Base qui regroupe tous tes modèles
 
+# Importation de ton Base SQLAlchemy (d'où viennent les modèles)
+from app.models.base import Base  # Adapte selon ton projet
+from app.models.message import Message
+from app.models.user import User
 
+# Cette ligne récupère les métadonnées de ta base SQLAlchemy
+target_metadata = Base.metadata
 
-from app.models.base import Base  # Importation de la classe Base
-from app.models.user import User  # Importation du modèle User
-
-
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
 config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-target_metadata = Base.metadata
-
 def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -24,19 +29,27 @@ def run_migrations_offline() -> None:
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
+
     with context.begin_transaction():
         context.run_migrations()
 
+
 def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
+
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
         with context.begin_transaction():
             context.run_migrations()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
